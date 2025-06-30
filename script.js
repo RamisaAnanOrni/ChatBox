7e8448b5ce7a43297d00369199c5b3278b906114
RamisaAnanOrni <137389854+RamisaAnanOrni@users.noreply.github.com>
2025-07-01 00:37:57 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function login() {
+  alert("Login button clicked!");
+  // Redirect: window.location.href = 'login.html';
+}
+
+function signup() {
+  alert("Sign Up button clicked!");
+  // Redirect: window.location.href = 'signup.html';
+}
